01a64361a4f481403ab691c9d172b283a13f9df5
aureliony <39163684+aureliony@users.noreply.github.com>
2024-01-21 01:35:29 +0800

    Fix mute toggle

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -4,19 +4,15 @@ import { Howl, Howler } from 'howler';
 import { useEffect, useState } from 'react';
 import { SoundFilled, PauseOutlined } from "@ant-design/icons";
 
+const sound = new Howl({
+    src: ['/bgm.mp3'],
+    autoplay: false,
+    loop: true,
+    volume: 0.5,
+});
+
 export default function NavBar() {
     const [isPlaying, setIsPlaying] = useState(true)
-
-    const sound = new Howl({
-        src: ['/bgm.mp3'],
-        autoplay: false,
-        loop: true,
-        volume: 0.5,
-        onend: () => {
-            console.log('Finished playing');
-        }
-        });
-
     const handlePlay = () => {
         if (isPlaying) {
             sound.pause();
@@ -26,14 +22,12 @@ export default function NavBar() {
             setIsPlaying(true)
         }
     }
-
     useEffect(() => {
         sound.play();
         return () => {
             sound.stop()
         }
     }, [])
-
     return (
         <div className="flex flex-row justify-end sticky w-full bg-blue-500 text-white p-4">
             {isPlaying ? (
@@ -41,9 +35,6 @@ export default function NavBar() {
             ): (
                 <PauseOutlined className="text-2xl" onClick={() => handlePlay()} />
             )}
-            
         </div>
     )
-
-        
 }
